98ed70eaa222941ae807eb7ffd1268b6ba30e775
RonaldKiprotich <protich12@gmail.com>
2020-08-31 13:54:42 +0300

    modified page

--- js/script.js
+++ js/script.js
@@ -3,30 +3,33 @@ $(document).ready(function(){
       $("#design-image").toggle();
       $("#descDesign").toggle();
     });
-  });
-  $(document).ready(function(){
     $(".development").click(function(){
       $("#development-image").toggle();
       $("#development").toggle();
     });
-  });
-  $(document).ready(function(){
     $(".product").click(function(){
       $("#product-image").toggle();
       $("#product").toggle();
     });
-  });
-
-  $(document).ready(function(){
-    $("form#message-form").submit(function(){
-      var name = $("input#input1").val();
-      if ($("input#input1").val() && $("input#input2").val()){
-        alert(name + ",thank you for reaching out to us. We will get back to you soon.");
-      }
-      else {
-        alert("Please enter your name and email address!");
-      }
-      
+      $("#work").mouseover(function() {
+        $("#overlay").show();
+    }).mouseout(function() {
+        $("#overlay").hide();
     });
-  
+
   });
+ 
+
+  // $(document).ready(function(){
+  //   $("form#message-form").submit(function(){
+  //     var name = $("input#input1").val();
+  //     if ($("input#input1").val() && $("input#input2").val()){
+  //       alert(name + ",thank you for reaching out to us. We will get back to you soon.");
+  //     }
+  //     else {
+  //       alert("Please enter your name and email address!");
+  //     }
+      
+  //   });
+  
+  // });
