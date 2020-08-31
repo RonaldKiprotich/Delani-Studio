98661afd10c48f78fe40243c566d632b70f47b66
RonaldKiprotich <protich12@gmail.com>
2020-08-31 08:14:21 +0300

    add alert

--- js/script.js
+++ js/script.js
@@ -17,10 +17,16 @@ $(document).ready(function(){
     });
   });
 
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
-    $("#overlay").show();
-  }).mouseout(function(){
-    $("#overlay").hide();
+  $(document).ready(function(){
+    $("form#message-form").submit(function(){
+      var name = $("input#input1").val();
+      if ($("input#input1").val() && $("input#input2").val()){
+        alert(name + ",thank you for reaching out to us. We will get back to you soon.");
+      }
+      else {
+        alert("Please enter your name and email address!");
+      }
+      
+    });
+  
   });
-});
