02e8e33479da7302902b36b54ade121ed10ac8ab
RonaldKiprotich <protich12@gmail.com>
2020-08-31 14:42:27 +0300

    add hover effect

--- js/script.js
+++ js/script.js
@@ -11,12 +11,63 @@ $(document).ready(function(){
       $("#product-image").toggle();
       $("#product").toggle();
     });
-      $("#work").mouseover(function() {
-        $("#overlay").show();
-    }).mouseout(function() {
-        $("#overlay").hide();
-    });
+//       $("#work1").mouseover(function() {
+//         $("#overlay").show();
+//     }).mouseout(function() {
+//         $("#overlay").hide();
+//     });
+//     $("#work2").mouseover(function() {
+//       $("#overlay").show();
+//   }).mouseout(function() {
+//       $("#overlay").hide();
+//   });
+//   $("#work3").mouseover(function() {
+//     $("#overlay").show();
+// }).mouseout(function() {
+//     $("#overlay").hide();
+// });
 
+
+$("#work1").mouseover(function(){
+  $("#overlay").show();
+}).mouseout(function(){
+  $("#overlay").hide();
+});
+$("#work2").mouseover(function(){
+  $("#overlay2").show();
+}).mouseout(function(){
+  $("#overlay2").hide();
+});
+$("#work3").mouseover(function(){
+  $("#overlay").show();
+}).mouseout(function(){
+  $("#overlay3").hide();
+});
+$("#work4").mouseover(function(){
+  $("#overlay4").show();
+}).mouseout(function(){
+  $("#overlay4").hide();
+});
+$("#work5").mouseover(function(){
+  $("#overlay5").show();
+}).mouseout(function(){
+  $("#overlay5").hide();
+});
+$("#work6").mouseover(function(){
+  $("#overlay6").show();
+}).mouseout(function(){
+  $("#overlay6").hide();
+});
+$("#work7").mouseover(function(){
+  $("#overlay7").show();
+}).mouseout(function(){
+  $("#overlay7").hide();
+});
+$("#work8").mouseover(function(){
+  $("#overlay8").show();
+}).mouseout(function(){
+  $("#overlay8").hide();
+});
   });
  
 
